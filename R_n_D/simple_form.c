20c7ab6103a1d93c97104ec70939b448043e2ad9
Tommy Lincoln <pajamapants3000@gmail.com>
2015-12-21 15:25:21 -0800

    Working on form test

--- R_n_D/simple_form.c
+++ R_n_D/simple_form.c
@@ -5,9 +5,22 @@
 
 #include <ncurses.h>
 #include <form.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+
+#define INI_NCRS initscr(); cbreak(); noecho();
 
 #define MAXLEN 20
 #define Question "Hello! My name is Tommy, what's yours?"
+
+// type fits return types of field_buffer, which will be called by set_name
+//+and forwarded back to its caller.
+// Copies contents of buffer to username variable, cropping whitespace;
+signed short set_name_no_space(char username[], FIELD* field);
+signed short set_name_as_is(char username[], FIELD* field);
+bool process_input(char ch, FORM* prompt_for_name)
+bool isspace(char ch);
 
 int main(void)
 {
@@ -16,3 +29,33 @@ int main(void)
 }
 
 
+signed short set_name_no_space(char username[], FIELD* field)
+{
+    char* buffer = field_buffer(field, 0);  // point to the field buffer!
+    signed short retval = errno;    // store any error value from field_buffer
+
+    if (buffer == NULL)
+        return retval;              // field_buffer encountered an error
+
+    unsigned short i = 0;           // fixed buffer size - keep track of pos
+    while (isspace(*(buffer + i)))
+        ++i;
+    do username[i] = *(buffer + i);
+    while (i < MAXLEN && !isspace(*(buffer + i++)));
+
+    return 0;
+}
+signed short set_name_as_is(char username[], FIELD* field)
+{
+    char* buffer = field_buffer(field, 0);  // point to the field buffer!
+    signed short retval = errno;    // store any error value from field_buffer
+
+    if (buffer == NULL)
+        return retval;              // field_buffer encountered an error
+
+    unsigned short i = 0;           // fixed buffer size - keep track of pos
+    do username[i] = *(buffer + i);
+    while (++i < MAXLEN)
+
+    return 0;
+}
