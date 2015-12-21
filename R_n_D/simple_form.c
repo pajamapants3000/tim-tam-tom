529d3d0fe06004f67c10144fef9481c1c217f9b7
Tommy Lincoln <pajamapants3000@gmail.com>
2015-12-21 05:24:53 -0800

    Started form experiment/test

---
+++ R_n_D/simple_form.c
@@ -0,0 +1,18 @@
+// simple_form.c
+// A simple form is presented to the user, asking for their name; once
+//+they have submitted an acceptable string, a message says hi and then the
+//+program exits.
+
+#include <ncurses.h>
+#include <form.h>
+
+#define MAXLEN 20
+#define Question "Hello! My name is Tommy, what's yours?"
+
+int main(void)
+{
+
+    return 0;
+}
+
+
